755fb1e4f7ac7a9abe2d59380e3605e9cdcb9e2c
luonghoangkim <akashi192002@gmail.com>
2023-10-13 17:06:01 +0700

    state user

--- src/components/social_media_login_bar.tsx
+++ src/components/social_media_login_bar.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 import FormRegister from './app.formRegister';
 import FormLogin from './app.formLogin';
 import { ButtonGroup, Dropdown, DropdownButton } from 'react-bootstrap';
+import { useSelector  } from 'react-redux';
 
 const SocialMediaLoginBar = () => {
     const [showRegister, setShowRegister] = useState(false);
     const [showLogin, setShowLogin] = useState(false); 
-    
-
+    const user = useSelector((state: any) => state.user) 
     const handleRegisterClick = () => {
         setShowRegister(true);
         setShowLogin(false);
@@ -27,31 +27,34 @@ const SocialMediaLoginBar = () => {
     return (
         <div className="flex items-center justify-end bg-black h-7 p-2 pr-10">
             <div className="text-white">
-               
-                     {/* <>
-                      <ButtonGroup>
-                        <DropdownButton as={ButtonGroup} variant="dark" title={`Xin chào: ${userEmail}`} id="bg-nested-dropdown">
-                            <Dropdown.Item eventKey="1" >Profile</Dropdown.Item>
-                            <Dropdown.Item eventKey="2">Đăng xuất</Dropdown.Item>
-                            <Dropdown.Item eventKey="2">Contact Us</Dropdown.Item>
-                        </DropdownButton>
-                    </ButtonGroup>
-                     <Link href="#">
-                            <span className="text-white no-underline hover:bg-gray-700">  Contact Us</span>
-                        </Link>
-                     </>    */}
+                {user?.name ? (
+                    <div>
+                    <ButtonGroup>
+                    <DropdownButton as={ButtonGroup} variant="dark" title={`Xin chào: ${user?.name}`} id="bg-nested-dropdown">
+                        <Dropdown.Item eventKey="1" >Profile</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">Đăng xuất</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">Contact Us</Dropdown.Item>
+                    </DropdownButton>
+                </ButtonGroup>
+                 <Link href="#">
+                        <span className="text-white no-underline hover:bg-gray-700">  Contact Us</span>
+                    </Link> 
+                    </div>
+                ):(
                     <div className="ml-auto flex items-center space-x-2">
-                        <button className=" text-white no-underline hover:bg-gray-700  " onClick={handleLoginClick}>
-                            Đăng nhập
-                        </button>
-                        <button className="text-white no-underline pr-2 hover:bg-gray-700" onClick={handleRegisterClick}>
-                            Đăng ký
-                        </button>
-                        <Link href="#">
-                            <span className="text-white no-underline hover:bg-gray-700">Contact Us</span>
-                        </Link>
-                    </div> 
-            </div>
+                    <button className=" text-white no-underline hover:bg-gray-700  " onClick={handleLoginClick}>
+                        Đăng nhập
+                    </button>
+                    <button className="text-white no-underline pr-2 hover:bg-gray-700" onClick={handleRegisterClick}>
+                        Đăng ký
+                    </button>
+                    <Link href="#">
+                        <span className="text-white no-underline hover:bg-gray-700">Contact Us</span>
+                    </Link>
+                </div>  
+                )}  
+              
+    </div>
             <FormRegister show={showRegister}
                 handleCloseForm={handleCloseForm}
                 toggleForm={handleLoginClick} />
@@ -59,7 +62,6 @@ const SocialMediaLoginBar = () => {
                 handleCloseForm={handleCloseForm}
                 toggleForm={handleRegisterClick}  />
         </div>
-    );
-};
-
+ )
+}
 export default SocialMediaLoginBar;
